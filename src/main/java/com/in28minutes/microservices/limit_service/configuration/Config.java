727b25b1fc43734eda7ad4773919c4c7bd1c1a03
prajwalsdhanke <prajwalsdhanke@gmail.com>
2024-10-13 21:00:54 +0530

    correcting service name

--- src/main/java/com/in28minutes/microservices/limit_service/configuration/Config.java
+++ src/main/java/com/in28minutes/microservices/limit_service/configuration/Config.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties("limit-service")
+@ConfigurationProperties("limits-service")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
